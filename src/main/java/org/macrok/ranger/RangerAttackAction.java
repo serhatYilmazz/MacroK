82889105246be35534dbd1817c99f03e2cdbdd3c
serhatYilmazz <yilmazserhat030@gmail.com>
2021-10-19 23:18:06 +0300

    Action implementations for Ranger

---
+++ src/main/java/org/macrok/ranger/RangerAttackAction.java
@@ -0,0 +1,16 @@
+package org.macrok.ranger;
+
+import org.macrok.action.AttackAction;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+
+@Service
+public class RangerAttackAction implements AttackAction {
+
+    @Override
+    public void act(Robot robot) {
+
+    }
+
+}
---
+++ src/main/java/org/macrok/ranger/RangerDefenseAction.java
@@ -0,0 +1,15 @@
+package org.macrok.ranger;
+
+import org.macrok.action.DefenseAction;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+@Service
+public class RangerDefenseAction implements DefenseAction {
+
+    @Override
+    public void act(Robot robot) {
+
+    }
+
+}
